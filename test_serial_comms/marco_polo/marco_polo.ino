a61a1294ce038031d53dba58552f13ddb53bc992
danieldkato <daniel.d.kato@gmail.com>
2016-03-22 10:54:19 -0400

    Make marco_polo.py an executable command line script
    This commit makes it possible to just call python marco_polo.py String from the command line, where String is a test string to send to the Arduino.

--- test_serial_comms/marco_polo/marco_polo.ino
+++ test_serial_comms/marco_polo/marco_polo.ino
@@ -1,41 +1,33 @@
 /*Minimal sketch for reading serial input, appending "ACK" 
 (for "acknowledge"), then echoing back to computer.*/
-//#include <Tone.h>
-
-String inputString="";
 String acknowledgeString = "ACK";
-boolean stringComplete=false;
-int speakerPin=13;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
-  pinMode(speakerPin,OUTPUT);
-  //testTones[0].begin(speakerPin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  serialEvent();
-  
-  if(stringComplete){
-    Serial.println(inputString);
-    inputString="";
-    stringComplete=false;
-  }
+  Serial.println(readLine());
 }
 
-void serialEvent(){
-  while(Serial.available()){
-    char inChar=Serial.read();
-    if (inChar!='\n'){
-      inputString+=inChar;  
-    }
-    else {
-      inputString+=acknowledgeString+"\n";
-      stringComplete=true;
+String readLine(){
+  String inputString="";
+  boolean stringComplete=false;
+  
+  while(stringComplete==false){
+    if(Serial.available()){
+      char inChar=Serial.read();
+      if (inChar!='\n'){
+        inputString+=inChar;  
+      }
+      else {
+        inputString+=acknowledgeString+"\n";
+        stringComplete=true;
+      }
     }
   }
+  return inputString;
 }
 
--- test_serial_comms/marco_polo/marco_polo.py
+++ test_serial_comms/marco_polo/marco_polo.py
@@ -6,28 +6,33 @@ import serial
 import time
 import sys
 
-print('marco.py start')
-testStr = 'marco' #Define test string here
+def marco(inputStr):
+	print('marco.py start')
+	#testStr = 'marco' #Define test string here
 
-#Initialize serial port connection
-connected = False
-ser = serial.Serial('COM3',9600,timeout=2)
+	#Initialize serial port connection
+	connected = False
+	ser = serial.Serial('COM3',9600,timeout=2)
 
-#Wait to receive signal that handshake is complete
-while not connected:
-	serin = ser.readline()
-	connected = True
-	print('Serial port open')
-	
-#Write a test string; Python 3.4 and greater requires strings to be converted to bytes
-if sys.version_info >= (3, 4):
-    ser.write(bytes(testStr+'\n','UTF-8'))
-else:
-    ser.write( testStr + '\n' )
+	#Wait to receive signal that handshake is complete
+	while not connected:
+		serin = ser.readline()
+		connected = True
+		print('Serial port open')
+		
+	#Write a test string; Python 3.4 and greater requires strings to be converted to bytes
+	if sys.version_info >= (3, 4):
+	    ser.write(bytes(inputStr+'\n','UTF-8'))
+	else:
+	    ser.write( testStr + '\n' )
 
-#Read echo back from Arduino
-print(ser.readline())
+	#Read echo back from Arduino
+	print(ser.readline())
 
-#Clean up
-ser.close()
-print('Done')
+	#Clean up
+	ser.close()
+	print('Done')
+
+if __name__ == "__main__":
+	import sys
+	marco(sys.argv[1])
