e3a7021f0ae013c07c3cc838fa41ae911cae8c36
danieldkato <daniel.d.kato@gmail.com>
2016-06-13 16:55:27 -0400

    Edit comments for test_monitor_black.py

--- test_monitor/test_monitor_black.py
+++ test_monitor/test_monitor_black.py
@@ -3,6 +3,29 @@
 Created on Thu Dec 10 17:25:28 2015
 
 @author: Acer
+
+Last updated DDK 6/7/16
+
+OVERVIEW:
+This script displays a black screen. Use to test monitor luminance, calibrate brightness-luminance curve, etc. 
+
+REQUIREMENTS:
+`python` >=2.7
+`psychopy`
+
+Make sure that the `monitor` object referred to in the definition of `mywin` has been defined in the local instance of psychopy.
+
+
+INSTRUCTIONS:
+Ensure that the `screen` attribute specified in the assignment of `mywin` is set to the appropriate monitor. Note that this is different from the `monitor` option; whereas `monitor` is an object that simply stores a number of data fields related to the monitor (like its model number, max resolution, dimensions, distance, etc.), `screen` is the actual hardware index used by the OS to identify the monitor. 
+
+In most cases, the stimulus will be presented on a secondary monitor dedicated to presenting visual stimuli, rather than on the main display. Indexing starts at 0, which will most likely be assigned to the main display, so indices for stimulus displays will probably start at 1.   
+
+Navigate to the directory containing this script in a command line window and enter:
+
+`python test_monitor_black.py`
+
+or hit the `run` button in an IDE like Spyder. 
 """
 
 from psychopy import visual, core, event #import some libraries from PsychoPy
