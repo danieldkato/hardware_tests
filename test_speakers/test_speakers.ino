ee115515162dc302e66cc23cd29d4a58b1b85d76
danieldkato <daniel.d.kato@gmail.com>
2016-07-05 12:27:49 -0400

    In test_speakers.ino, use Arduino core library tone() and noTone() functions instead of Tone library
    Now that the devices library is using these functions instead, this test should use them as well.

--- test_speakers/test_speakers.ino
+++ test_speakers/test_speakers.ino
@@ -37,18 +37,17 @@
  * Upload to the Arduino microcontroller. 
  */
 
-#include <Tone.h>
+#include <Arduino.h>
 
 boolean once = false;
 int spkrPin = 13;
-int duration = 2000;
+int duration = 1000;
 
-Tone cueTones[1];
+unsigned long start;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(spkrPin,OUTPUT);
-  cueTones[0].begin(spkrPin);
   randomSeed(analogRead(4));
 
   Serial.begin(9600);
@@ -57,14 +56,11 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if ( once == false){
-      int start = millis();
-      int now = millis();
-      while ( now - start < duration ){
-        int freq = random( 10000, 20000 );
-        cueTones[0].play(freq);
-        now = millis();
+      start = millis();
+      while ( millis() - start < duration ){
+        tone(spkrPin,random(10000,20000));
       }
     }
-  cueTones[0].stop();
+  noTone(spkrPin);
   once = true;
 }
