26326ba7416c3d82edb2fba6a3b52a342c80cd9e
danieldkato <daniel.d.kato@gmail.com>
2016-12-16 12:25:32 -0500

    Change test_HES.ino and test_stepper_HES.ino to work with stepper driver

--- test_stepper/test_HES/test_HES.ino
+++ test_stepper/test_HES/test_HES.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 
-const int HALL_PIN = 1;
+const int HALL_PIN = 0;
 
 void setup() {
   // put your setup code here, to run once:
--- test_stepper/test_stepper_HES/test_stepper_HES.ino
+++ test_stepper/test_stepper_HES/test_stepper_HES.ino
@@ -32,53 +32,55 @@
 #include <Stepper.h>
 
 //Declare stepper constants and variables
-const int MOTOR_STEPS = 200;
-const int STPR1_PIN1 = 8;
-const int STPR1_PIN2 = 6;
-const int STPR1_ENBL = 7;
-const int HALL_PIN = 1;
+const int STP_PIN = 6;
+const int DIR_PIN = 8;
+const int HALL_PIN = 0;
+int stepHalfDelay = 4000; // microseconds
+int microstep = 8;
 int hall_thresh = 50;
 int hall_val = 500;
-int stepCCW = -50;
 long period = 2000;
-
-//Instantiate stepper object
-Stepper stpr1(MOTOR_STEPS, STPR1_PIN1, STPR1_PIN2) ;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(STPR1_PIN1, OUTPUT);
-  pinMode(STPR1_PIN2, OUTPUT);
-  pinMode(STPR1_ENBL, OUTPUT);
-  digitalWrite(STPR1_ENBL, LOW);
-  stpr1.setSpeed(100);
+  pinMode(STP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  stpr1Fwd();
+  fwd();
   delay(period/2);
-  stpr1Back();
+  back();
   delay(period/2);
     
 }
 
-void stpr1Fwd(){
-  digitalWrite(STPR1_ENBL, HIGH);
-  delay(100);
-  hall_val = analogRead(HALL_PIN);
-  while ( hall_val > hall_thresh) {
-    stpr1.step(1);
-    delay(1);  // slight delay after movement to ensure proper step before next
-    hall_val = analogRead(HALL_PIN);
-  }
+void rotate_one_step()
+{
+  digitalWrite(STP_PIN, HIGH);
+  delayMicroseconds( stepHalfDelay / microstep );
+  digitalWrite(STP_PIN, LOW);
+  delayMicroseconds( stepHalfDelay / microstep );
 }
 
-void stpr1Back(){
-  stpr1.step(stepCCW);
-  delay(200);
-  digitalWrite(STPR1_ENBL, LOW);
+void fwd(){
+  digitalWrite(DIR_PIN, HIGH);
+  hall_val = analogRead(HALL_PIN);
+  while(hall_val>hall_thresh){
+    rotate_one_step(); //how to deal with direction??
+    delay(1);
+    hall_val = analogRead(HALL_PIN);
+  }
+  Serial.println("stepper extended");
+}
+
+void back(){
+  digitalWrite(DIR_PIN, LOW);
+  delay(1);
+  for(int i = 0; i < 50; i++){rotate_one_step(); delay(1);}
+  Serial.println("stepper retracted");
 }
 
 
