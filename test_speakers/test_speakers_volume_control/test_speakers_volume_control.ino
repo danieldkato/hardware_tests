4f2dac2688c806f1746fadb50d7078cb7693f4af
danieldkato <daniel.d.kato@gmail.com>
2017-05-22 16:16:04 -0400

    Add code in test_speakers_volume_control.ino for testing how standard Arduino delay() function works when the Volume library is being used

--- test_speakers/test_speakers_volume_control/test_speakers_volume_control.ino
+++ test_speakers/test_speakers_volume_control/test_speakers_volume_control.ino
@@ -21,6 +21,8 @@
  * There must be a resistor in line between the Arduino output pin 
  * and the speaker input terminal.
  * 
+ * 
+ * 
  * As of 160507, if running this sketch on hs05bruno8 
  * (the Dell T3400 in the room 504), additional steps must
  * be taken in order to compile. The computer uses a 
@@ -48,14 +50,23 @@
  * As of 170523, this doesn't seem to work great at
  * lower volumes - discrete pulses are clearly 
  * discernible.
+ * 
+ * IMPORTANT NOTE: once you've called begin() on a Volume object,
+ * you MUST resort the library's vol.dely(), vol.millis(), 
+ * vol.micros(), etc. in place of the standard Arduino 
+ * timekeeping functions. This is because beginning the Volume
+ * object increases the speed of Timer0, which is used for the 
+ * standard Arduino timekeeping functions. For more information,
+ * see https://github.com/connornishijima/arduino-volume1#limitations
  */
 
 #include <Arduino.h>
 #include "Volume.h"
 
 boolean once = false;
-int duration = 1000;
+int duration = 500;
 unsigned long start;
+byte volumes[4] = {255, 127, 12, 0};
 Volume vol;
 
 void setup() {
@@ -65,12 +76,36 @@ void setup() {
 }
 
 void loop(){
-    byte volumes[4] = {255, 127, 12, 0};
+    
+    vol.noTone();  
+    /* ^ This line is here to stop the final call to tone() 
+     * at the end of the 'if' block; for some reason it stops 
+     * the tone from EVER playing if it's placed inside or 
+     * after the 'if' block, but it seems to work properly if 
+     * it's called at the beginning of the next pass of loop()
+     */
+    
     if (once == false){
+
+      // Sweep through four different volumes
       for(int i = 0; i < 4; i++){
         vol.tone(440, volumes[i]);
         vol.delay(duration);
       }
-    once = true;
+
+      /* Insert a delay here to show how the standard
+       * Arduino delay() function doesn't work properly 
+       * once vol.begin() has been called, and that vol.delay()
+       * must be called instead
+      */
+      //delay(3000); // this will wait for only 1/64th as long as the delay specified in the input argument
+      vol.delay(3000); // this will wait for the amount of time specified in the input argument
+      
+
+      // Play a probe tone to indicate when the delay is done
+      vol.tone(440, volumes[0]);
+      vol.delay(duration);
+    
+      once = true;
     }
 }
