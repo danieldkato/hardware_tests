b3820ca91d9f6e8f8f8620bea67aa8edad2cb4ac
danieldkato <daniel.d.kato@gmail.com>
2017-05-22 14:10:03 -0400

    Add sketch that does nothing, use to get Arduino to stop running all other programs

---
+++ do_nothing/do_nothing/do_nothing.ino
@@ -0,0 +1,13 @@
+/* A sketch that does nothing. Upload to an Arduino to
+ * make it stop running all other programs.
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
