3bb5b638b9bc9e32790043d46719709c107fdba0
danieldkato <lab@hs05bruno8.cpmc.columbia.edu>
2017-05-24 13:47:07 -0400

    Create Speaker.m and Speakers.mat
    Decided that rather than having speaker parameters defined in get_speaker_spectrogram.m, they should be defined in a separate .mat file that can then be loaded. I decided to create a speaker class, and have all speakers saved in an object array that can be loaded by get_speaker_spectrogram.m. Also, the function for checking that a speaker's paramters are compatible with the requested stimulus is now method of the speaker class; the idea is that get_speaker_spectrogram will load the Speaker objects, find the one whose name matches that speaker name given as an input argument, then call that speaker's validation method with the requested stimulus parameters as inputs.

---
+++ test_Speakers/get_speaker_spectrogram/Speaker.m
@@ -0,0 +1,22 @@
+classdef Speaker
+    properties
+        Name
+        MinF
+        MaxF
+    end
+    methods
+        function obj = Speaker(name, min, max)
+            obj.Name = name;
+            obj.MinF = min;
+            obj.MaxF = max;
+        end
+        function eval(obj, tgtMin, tgtMax)
+            if (tgtMin < obj.MinF)
+                warning('Desired minimum stimulus frequency outside of speaker range.');
+            end
+            if (tgtMax > obj.MaxF)
+                warning('Desired maximum stimulus frequency outside of speaker range.');
+            end
+        end
+    end
+end
