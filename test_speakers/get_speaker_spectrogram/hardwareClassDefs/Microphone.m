fd4bb0084025a3f8bd73ffd56d759fb79555d302
danieldkato <daniel.d.kato@gmail.com>
2017-06-08 12:28:24 -0400

    Move Microphone.m to hardwareClassDefs subdirectory

---
+++ test_speakers/get_speaker_spectrogram/hardwareClassDefs/Microphone.m
@@ -0,0 +1,17 @@
+classdef Microphone
+    properties
+        Mfr % manufacturer
+        Mdl % model #
+        Description
+        MinF % minimum frequency (within +/- 1 dB of max response), in Hz
+        MaxF % maximum frequency (within +/- 1 dB of max response), in Hz
+        Sensitivity % mV/Pa
+    end
+    methods
+        function obj = Microphone(mdl, min, max)
+            obj.Mdl = mdl;
+            obj.MinF = min;
+            obj.MaxF = max;
+        end
+    end
+end
