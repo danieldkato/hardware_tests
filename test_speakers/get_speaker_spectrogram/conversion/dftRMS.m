4cd7f8413824da3639093dbed18a7f7a6f09b070
danieldkato <daniel.d.kato@gmail.com>
2017-06-21 12:59:06 -0400

    Update documentation for dftRMS.m

--- test_speakers/get_speaker_spectrogram/conversion/dftRMS.m
+++ test_speakers/get_speaker_spectrogram/conversion/dftRMS.m
@@ -11,8 +11,8 @@ function f2rms = dftRMS(Recording)
 % I. OVERVIEW
 % This function takes an audio recording in the time domain and returns its
 % Fourier tansform in terms of pascals RMS. In other words, for each
-% frequency, it gives the rms amplitude of the sinusoid of that frequency
-% that would be needed to reconstruct the original stimulus.
+% frequency, it gives the amplitude in pascals rms of the sinusoid of that
+% frequency that would be needed to reconstruct the original stimulus.
 
 
 % II. REQUIREMENTS
@@ -22,16 +22,21 @@ function f2rms = dftRMS(Recording)
 % III. INPUTS
 % 1) Recording - a struct containing data and metadat from an audio
 % recording. It must have at least the following fields:
-%   Data % actual recording data
-%   TrueSampleRate % actual sample rate, in Hz, of recording 
-%   PreStimDuration.val % pre-stimulus period duration, in seconds (can be 0)
-%   PostStimDuration.val % post-stimulus period duration, in seconds (can be 0)
-%   Microphone % model number of microphone used to make recording
+%   a) Data - actual recording data
+%   b) TrueSampleRate - actual sample rate, in Hz, of recording 
+%   c) PreStimDuration.val - pre-stimulus period duration, in seconds (can be 0)
+%   d) PostStimDuration.val - post-stimulus period duration, in seconds (can be 0)
+%   e) Microphone - model number of microphone used to make recording (necessary for 
+%      retrieving the mic's sensitivity, which specifies the conversion from Volts 
+%      to pascals)
 
 
 % IV. OUTPUTS
-% 1) f2rms - a 1 x N vector where the i-th element specifies the RMS
-% amplitude of the i-th frequency component of the original signal. 
+% 1) f2rms - a structure with the following fields:
+%   a) Amplitudes - 1 x N vector where the i-th element specifies the RMS
+%      amplitude of the i-th frequency component of the original signal.
+%   b) Frequencies - 1 x N vector of the frequencies associated with each
+%      frequency component, in Hz
 
 
 % TODO:
@@ -39,26 +44,34 @@ function f2rms = dftRMS(Recording)
 % 2) Provide alternative syntax to accept non-struct, non-HDF5 inputs?
 
 
-%% Load voltage trace and get recording metadata    
-pascals = volts2pascals(Recording); % convert voltage to pascals
+%% Pre-processing:
 
-preStimDur = Recording.PreStimDuration.val; % get pre-stimulus duration to identify semgent of trace corresponding to stimulus
-postStimDur = Recording.PostStimDuration.val; % get post-stimulus duration to identify semgent of trace corresponding to stimulus
+% convert voltage to pascals:
+pascals = volts2pascals(Recording); 
+
+% extract data from just the stimulus period:
+preStimDur = Recording.PreStimDuration.val; 
+postStimDur = Recording.PostStimDuration.val; 
 P = pascals(ceil(preStimDur*Recording.TrueSampleRate.val):length(Recording.Data) - ceil(postStimDur * Recording.TrueSampleRate.val));
 
-% want to make this even-lengthed if it isn't already
+% plot the stimulus in pascals to make sure it looks reasonable:
+times = 1/Recording.TrueSampleRate.val * (1:length(P));
+
+% make time-series even-lengthed if it isn't already:
 if mod(length(P),2) ~= 0
     P = P(1:end-1);
 end
 N = length(P);
 
+
+%% Fourier analysis:
+
 % Get DFT of pressure trace using fft(). In order to convert this to an
-% peak-to-peak *amplitude* spectrum (as opposed to a power spectrum), take
-% abs(fft(x)/N). In other words, if you add sine waves of frequency F and G
-% with amplitudes A and B, respectively, and want to obtain a transform
-% where the F-th frequency component will be A, and the G-th frequency
-% component will be B, then take abs(fft(x)/N). See documentation for fft()
-% at https://www.mathworks.com/help/matlab/ref/fft.html for illustration. 
+% peak-to-peak *amplitude* spectrum (as opposed to a *power* spectrum),
+% take abs(fft(x)/N). In other words, the value of the F-th element of
+% abs(fft(x)/N) is the peak-to-amplitude of the associated frequency needed
+% to reconstruct the original signal. See documentation for fft() at
+% https://www.mathworks.com/help/matlab/ref/fft.html for illustration.
 DFT = fft(P);
 DFT = abs(DFT/N); 
 
@@ -84,6 +97,11 @@ DFT(2:end-1) = 2*DFT(2:end-1);
 % divided by sqrt(2):
 % https://en.wikipedia.org/wiki/Root_mean_square
 % https://dsp.stackexchange.com/questions/14808/spl-values-from-fft-of-microphone-signal
-f2rms = DFT/sqrt(2);
+f2rms.Amplitudes = DFT/sqrt(2);
+
+% Get the actual frequencies, in Hz, that each of these components
+% correspond to 
+f = (0:length(f2rms.Amplitudes)-1) * (Recording.TrueSampleRate.val/N);
+f2rms.Frequencies = f;
 
 end
