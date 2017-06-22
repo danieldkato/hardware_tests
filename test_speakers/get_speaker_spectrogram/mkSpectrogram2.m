877075f13cac202786b3374d140f68449539ba2d
danieldkato <daniel.d.kato@gmail.com>
2017-06-22 12:56:25 -0400

    mkSpectrogram2.m

--- test_speakers/get_speaker_spectrogram/mkSpectrogram2.m
+++ test_speakers/get_speaker_spectrogram/mkSpectrogram2.m
@@ -7,8 +7,7 @@ function S = mkSpectrogram2(recordingPath, band1, band2, varargin)
 % IV. OUTPUTS
 
 
-%% 
-% I. OVERVIEW
+%% I. OVERVIEW
 % This function computes the scale factor needed to ensure that acoustic
 % noise played in one frequency band from a given speaker matches the
 % loudness of noise played in another frequency band from the same speaker.
@@ -17,19 +16,19 @@ function S = mkSpectrogram2(recordingPath, band1, band2, varargin)
 % frequency bands to aid in manual selection of reasonable frequency bands.
 
 
-% II. REQUIREMENTS
+%% II. REQUIREMENTS
 % 1) dftRMS.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
-%       a) This in turn requires volts2pascals.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
-% 2) pa2db.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
-% 3) db2pa.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
-% 4) Mics.mat, available github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/hardwareDefs
-% 5) audiogram_Heffner2002.mat, avaialable at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/audiograms
+% 2) volts2pascals.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
+% 3) pa2db.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
+% 4) db2pa.m, available at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/conversion
+% 5) Mics.mat, available github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/hardwareDefs
+% 6) audiogram_Heffner2002.mat, avaialable at github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/audiograms
 
 % Note that some of these requirements may have other dependencies of their own.
 % Also, see INPUTS below for input formatting requirements.
 
 
-% III. INPUTS
+%% III. INPUTS
 % 1) recordingPath - string specifying the location of a .mat file
 % containing a structure called `Recording`. This structure must include at
 % least the following fields:
@@ -54,63 +53,63 @@ function S = mkSpectrogram2(recordingPath, band1, band2, varargin)
 % 4) [optional] audiogramPath - string specifying the location of a .mat file
 % containing a structure called `Audiogram`. This structure must include at
 % least the following fields:
+
 %   a) ThreshDBSPL - a 1 x F vector of hearing thresholds, in dB SPL, where
-%   F is the number of frequencies for which hearing thresholds have been
-%   measured
+%      F is the number of frequencies for which hearing thresholds have been
+%      measured
 %   b) FreqKHz - a 1 x F vector of frequencies, in KHz, for which hearing
-%   thresholds have been measured. The frequency specified by a given
-%   element of FreqKHz must correspond to the hearing thresholds specified
-%   the corresponding element of ThreshDBSPL - in other words, the i-th
-%   element of ThreshDBSPL should be the hearing threshold at the frequency
-%   specified by the i-th element of FreqKHz
+%      thresholds have been measured. The frequency specified by a given
+%      element of FreqKHz must correspond to the hearing thresholds specified
+%      the corresponding element of ThreshDBSPL - in other words, the i-th
+%      element of ThreshDBSPL should be the hearing threshold at the frequency
+%      specified by the i-th element of FreqKHz
 
 % If no audiogramPath is specified, then the Audiogram structure saved in
 % `audiogram_Heffner2002.mat` will be used as a default. 
 
 
-%IV. OUTPUTS
+%% IV. OUTPUTS
 % 1) S, a structure with the following fields:
+
 %   a) ScaleFactor - the scalar by which the amplitude of any signal in the
-%   first frequency band must be multiplied in order to match the loudness
-%   of any signal in the second frequency band
-
+%      first frequency band must be multiplied in order to match the loudness
+%      of any signal in the second frequency band
 %   b) FrequenciesKHz - 1 x N vector specifying the frequencies, in KHz,
-%   for which the speaker's response function is defined. This function
-%   also linearly interpolates the audiogram data to define the audiogram
-%   for all of these frequencies as well. 
-
+%      for which the speaker's response function is defined. This function
+%      also linearly interpolates the audiogram data to define the audiogram
+%      for all of these frequencies as well. 
 %   c) DFT - structure containing information about the discrete Fourier
-%   transform of the recorded noise stimulus. This structure includes the
-%   following fields:
-%       i) FrequenciesHz - 1 x F vector of frequencies, in Hz, for which
-%       the discrete Fourier transform of the original recording is
-%       defined, where F is the number of frequencies in the DFT. Note that
-%       F is equal to the number of samples in the original time-series
-%       data.
-%       ii) AmplitudesPaRMS - 1 x F vector of amplitudes, in RMS pascals,
-%       associated with each frequency component of the original signal.
-%       That is, in order to reconstruct the original signal, then for all
-%       elements i of FrequenciesHz and AmplitudesPaRMS, you would need to
-%       add a sine wave with frequency FrequenciesHz(i) and
-%       AmplitudesPaRMS(i). 
-%       iii) AmplitudesDBSPL - same as AmplitudesPaRMS, but expressed in dB
-%       SPL. 
+%      transform of the recorded noise stimulus. This structure includes the
+%      following fields:
+
+%       - FrequenciesHz - 1 x F vector of frequencies, in Hz, for which
+%          the discrete Fourier transform of the original recording is
+%          defined, where F is the number of frequencies in the DFT. Note that
+%          F is equal to the number of samples in the original time-series
+%          data.
+%       - AmplitudesPaRMS - 1 x F vector of amplitudes, in RMS pascals,
+%         associated with each frequency component of the original signal.
+%         That is, in order to reconstruct the original signal, then for all
+%         elements i of FrequenciesHz and AmplitudesPaRMS, you would need to
+%         add a sine wave with frequency FrequenciesHz(i) and
+%         AmplitudesPaRMS(i). 
+%       - AmplitudesDBSPL - same as AmplitudesPaRMS, but expressed in dB
+%         SPL. 
 
 %   c) Band1 - structure defining the bounds of the first frequency band.
-%   This structure includes the following fields:
-%       i) FrequenciesKHz - 2-element array defining the upper and lower
-%       bounds of the first frequency band in KHz
-%       ii) indices - 2-element array defining the upper and lower
-%       bounds of the first frequency band as indices into the frequency
-%       domain defined in S.FrequenciesKHz
+%      This structure includes the following fields:
+%       - FrequenciesKHz - 2-element array defining the upper and lower
+%          bounds of the first frequency band in KHz
+%       - indices - 2-element array defining the upper and lower
+%         bounds of the first frequency band as indices into a 1 x F vector,
+%         where F is the number of frequencies sampled
 
 %   d) Band2 - same as Band1, but for the second frequency band
 
-%   e) Recording - string specifying the name of the .mat file containing
-%   the `Recording` struct used in the current analysis
+%   e) Recording - all metadata fields from struct `Recording` used in the current analysis
 
 %   f) Audiogram - string specifying the name of the .mat file containing
-%   the `Audiogram` struct used in the current analysis
+%   the struct `Audiogram` used in the current analysis
 
 
 %% Check if audiogram was defined, and if not, use default audiogram
@@ -121,7 +120,7 @@ if length(varargin)>0
     audiogramPath = varargin{1};
 end
 
-S.Audiogram = audiogramPath;
+S.AudiogramPath = audiogramPath;
 S.Speaker = 'unknown';
 
 %% Define desired frequency bands:
@@ -134,7 +133,7 @@ S.Band2.FrequenciesKHz = band2;
 
 %% Create periodogram of recording:
 load(recordingPath); 
-S.Recording = recordingPath;
+S.RecordingPath = recordingPath;
 if isfield(Recording, 'Speaker')
     S.Speaker = Recording.Speaker;
 end
@@ -190,13 +189,22 @@ p1 = patch([S.Band1.FrequenciesKHz(1) S.Band1.FrequenciesKHz(1) S.Band1.Frequenc
 p2 = patch([S.Band2.FrequenciesKHz(1) S.Band2.FrequenciesKHz(1) S.Band2.FrequenciesKHz(2) S.Band2.FrequenciesKHz(2)], recY, [0.95, 0.95, 0.25], 'FaceAlpha', 0.3, 'EdgeColor', 'none');
 
 % Label figure
-title(strcat(['Speaker ', S.Speaker, ' response chart & murine audiogram']));
+titleStr = {strcat(['Speaker ', S.Speaker, ' response chart & murine audiogram']);
+            strcat(['Response chart obtained from ', num2str(Recording.StimDur.val), '-', Recording.StimDur.units(1:end-1), ' white noise burst']);
+};
+
+title(titleStr);
 xlabel('Frequency (kHz)');
 ylabel('Volume (dB SPL)');
 agName = strcat([Audiogram.Authors{1}{1}, ' et al ', num2str(Audiogram.Year)]);
 legend([rawPeriodogram, audiogramPlot], {strcat(['Speaker ', S.Speaker, ' response chart']),
         strcat(['Murine audiogram (', agName, ')'])
-    });
+});
+
+%% Write some recording metadata to S 
+Recording = rmfield(Recording, 'Data');
+S.Recording = Recording;
+
 
 %{
 figure;
