ec626d16758cfb0052b5731c2d8e60b667366e42
danieldkato <daniel.d.kato@gmail.com>
2017-06-27 19:08:30 -0400

    Edit documentation for volts2pascals.m

--- test_speakers/get_speaker_spectrogram/conversion/volts2pascals.m
+++ test_speakers/get_speaker_spectrogram/conversion/volts2pascals.m
@@ -1,47 +1,43 @@
 function pascals = volts2pascals(Recording)
 
 % DOCUMENTATION TABLE OF CONTENTS
-% I. SYNATX
-% II. OVERVIEW
-% III. REQUIREMENTS
-% IV. INPUTS
-% V. OUTPUTS
+% I. OVERVIEW
+% II. REQUIREMENTS
+% III. INPUTS
+% IV. OUTPUTS
 
 
-%%
-% I. SYNTAX
-% pascals = volts2pascals(Recordings)
-
-
-% II. OVERVIEW
+%% I. OVERVIEW
 % This function takes a voltage trace from an audio recording and converts
 % it to a pressure trace.
 
 
-% III. REQUIREMENTS
+%% II. REQUIREMENTS
 % 1) Mics.mat - .mat file containing a MATLAB struct array called Mics,
 % each element of which represents a microphone and defines its
 % specifications. Each element of Mics must have at least the following
 % fields:
-%   Mdl - String stating the model number of the microphone
-%   Sensitivity - numeric value specifying the microphone's sensitivity in mV/Pa
+%   a) Mdl - String stating the model number of the microphone
+%   b) Sensitivity - numeric value specifying the microphone's sensitivity in mV/Pa
 
 % A Mics.mat file containing definitions for one commonly used microphone
-% is available a 
+% is available at:
+
+% https://github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/hardwareDefs
 
 
-% IV. INPUTS
+%% III. INPUTS
 % 1) Recording - MATLAB struct containing audio recording data and metadata.
 % It must contain at least the following fields:
 %   a) Data - actual recording data, in Volts, formatted as a 1 x N vector, where N is the number of samples in the recording.
 %   b) Microphone - String specifying the model number of the microphone used to make the recording
 
 
-% V. OUTPUTS
+%% IV. OUTPUTS
 % 1) pascals - 1 x N vector of pressure values, expressed in pascals
 
 
-% TODO:
+%% TODO:
 % 1) Provide alternative syntax for when Microphone.m and Mics.mat are not
 % available?
 % 2) Provide alternative syntax for non-MATLAB struct inputs?
