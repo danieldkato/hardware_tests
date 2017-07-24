a77393604873bb59bfe17542f469a181eb0b589d
danieldkato <lab@hs05bruno8.cpmc.columbia.edu>
2017-07-24 15:50:51 -0400

    Edit documentation for recordDigitalTriggeredAudioNoise.m

--- test_speakers/get_speaker_spectrogram/recordDigitalTriggeredAudioNoise/recordDigitalTriggeredAudioNoise.m
+++ test_speakers/get_speaker_spectrogram/recordDigitalTriggeredAudioNoise/recordDigitalTriggeredAudioNoise.m
@@ -62,8 +62,7 @@ function recordDigitalTriggeredAudioNoise(speaker, stimID, portID, configFile)
     
 %    4) National Instruments LabView systems engineering software.
     
-%    5) The LabView virtual instrument DigitalTriggeredAudioNoise.vi,
-%       available at
+%    5) The LabView virtual instrument DigitalTriggeredAudioNoise.vi, available at
 %       https://github.com/danieldkato/hardware_tests/tree/master/test_speakers/get_speaker_spectrogram/recordDigitalTriggeredAudioNoise
     
 %    6) struct2txt.m, available at https://github.com/danieldkato/utilities/blob/master/struct2txt.m
@@ -88,9 +87,16 @@ function recordDigitalTriggeredAudioNoise(speaker, stimID, portID, configFile)
 
 % 4) configFile - path to a MATLAB-evaluable .txt file defining a structure
 %    called `Recording`, which specifies various parameters necessary for
-%    setting up data acquisition. While this function supplies default values
-%    for all required fields, it is best practice to use a config file that
-%    defines the following:
+%    setting up data acquisition. This structure must supply the following fields:
+
+%       Recording.Arduino.Sketch.LocalPath - char array specifying the path of the main sketch to be run on the Arduino 
+%       Recording.Arduino.Board - char array specifying the model of the board to which the sketch will be uploaded. This 
+%                                 should have the syntax used by the Arduino command line interface. E.g., for an Arduino 
+%                                 Uno, the value should be 'arduino:avr:uno'. For more detail, see the Arduino CLI documentation at 
+%                                 https://github.com/arduino/Arduino/blob/master/build/shared/manpage.adoc
+
+%    In addition, it is best practice to use a config file that defines the 
+%    following fields, although this function will provide defaults if needed:
 %
 %       Recording.PreStimDuration.val - numeric value specifying duration of pre-stimulus period, in seconds
 %       Recording.PostStimDuration.val - numeric value specifying duration of post-stimulus period, in seconds
@@ -170,7 +176,10 @@ function recordDigitalTriggeredAudioNoise(speaker, stimID, portID, configFile)
 %   a) The product of all gains on any signal conditioners or amplifiers in
 %     line with the microphone. For example, if there is a signal conditioner
 %     with a gain of 10 in line with another amplifier with a gain of 50, set
-%     this to 500. If there is no signal conditioner, enter `1`. (we need this to recover the amplitude of the actual voltage signal put out by the microphone, which, along with the microphone spec sheet, can be used to infer the actual sound pressure level on the mic in Pa)
+%     this to 500. If there is no signal conditioner, enter `1`. (we need 
+%     this to recover the amplitude of the actual voltage signal put out by 
+%     the microphone, which, along with the microphone spec sheet, can be 
+%     used to infer the actual sound pressure level on the mic in Pa)
 
 %   b) The distance of the microphone from the speakers, in millimeters
 
@@ -194,7 +203,7 @@ function recordDigitalTriggeredAudioNoise(speaker, stimID, portID, configFile)
 % recording and generates a spectrogram of the recorded signal. 
 
 
-% TODO:
+%% TODO:
 % 1) Should ultimately update this to use session-based, rather than
 %    legacy DAQ interface (when we update MATLAB on ephys computer)
 
@@ -207,6 +216,12 @@ function recordDigitalTriggeredAudioNoise(speaker, stimID, portID, configFile)
 % 4) Add support for single-ended vs. differential input
 
 % 5) Should add a log of all warnings to metadata
+
+% 6) Should write a generic function that recursively checks if structure
+%    defined in config file has all necessary fields defined in default 
+%    structure; this way, I can include default values for sketch path and 
+%    board model in Defaults structure (this function currently does not
+%    check recursively)
 
 % Last updated DDK 2017-07-21
 
@@ -242,7 +257,7 @@ eval(content);
 
 % Validate that config file includes required settings; if not, throw a warning and use defaults
 for i = 1:length(requiredFields)
-    if ~isfield(Recording, requiredFields{i}) % If a field is missing entirely from the loaded condig structure...
+    if ~isfield(Recording, requiredFields{i}) % If a field is missing entirely from the loaded config structure...
         if isfield(Defaults.(requiredFields{i}), 'val') % ... and if the missing field is supposed to have separate value and units sub-fields...
             Recording.(requiredFields{i}).val = Defaults.(requiredFields{i}).val;
             Recording.(requiredFields{i}).units = Defaults.(requiredFields{i}).units;
