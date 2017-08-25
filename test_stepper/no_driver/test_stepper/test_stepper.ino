698ee96424a67d70cef9d1bf82a3d876171957f1
danieldkato <daniel.d.kato@gmail.com>
2017-08-24 20:03:26 -0400

    Edit comments for test_stepper.ino

--- test_stepper/no_driver/test_stepper/test_stepper.ino
+++ test_stepper/no_driver/test_stepper/test_stepper.ino
@@ -1,27 +1,45 @@
-/* Last updated DDK 6/7/16
+/* test_stepper.ino
  *  
- * OVERVIEW:
+ *  ////////////////////////////////////////////////////////
+ *  Documentation table of contents:
+ * I. OVERVIEW
+ * II. REQUIREMENTS
+ * III. INSTRUCTIONS
+ *  
+ * Last updated DDK 2017-08-24
+ *  
+ * ///////////////////////////////////////////////////////// 
+ * I. OVERVIEW:
+ * 
  * Simple sketch to make a stepper motor rotate some number 
- * of steps one time, independent of any hall effect sensors.
- * Use this to test that the stepper, the corresponding 
- * digital  output pins and the connections between are 
- * working.
+ * of steps once, independent of any hall effect sensors.
+ * Use this to test that the stepper and all associated digital 
+ * output pins and the connections are working.
  * 
  * Verified to work on Build3 160506.
  * 
  * 
- * REQUIREMENTS:
- * 1) Stepper motor connected to Arduino microcontroller in 2-pin configuration. 
+ * ////////////////////////////////////////////////////////////
+ * II. REQUIREMENTS:
  * 
- * The Arduino MUST be powered by ~5V; the stepper will 
- * otherwise draw too much current and the Arduino will
- * not be detected. 
+ * 1) Bipolar stepper motor connected to H-bridge (e.g. L293D) in 
+ *    2-pin configuration. For instructions on how to achieve this, see: 
+ *    http://www.tigoe.net/pcomp/code/circuits/motors/stepper-motors/
+ * 
+ * 2) Arduino microcrontroller configured to control H-bridge. See
+ *    link above for detailed instructions on circuit configuration. 
+ * 
+ * 3) The Arduino MUST be powered by ~5V; the stepper will otherwise 
+ *    draw too much current and the Arduino will not be detected. 
  *  
  * 
- * INSTRUCTIONS:
+ * ////////////////////////////////////////////////////////////
+ * III. INSTRUCTIONS:
+ * 
  * Ensure that the pins specified in this sketch match the
  * pins connected to the stepper motor. Upload the sketch to
  * the Arduino microcontroller. 
+ * 
  */
 
 #include <Arduino.h>
