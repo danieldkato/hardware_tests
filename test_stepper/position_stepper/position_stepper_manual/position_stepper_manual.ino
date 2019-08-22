62a49feba9cfc7427e978f02c9456b5cc34b961c
danieldkato <daniel.d.kato@gmail.com>
2019-08-22 14:16:42 -0400

    In position_stepper_manual.ino, assume that if input is not an empty string terminated in a newline character or q, then it is an int
    Will be up to host PC program to do input validation

--- test_stepper/position_stepper/position_stepper_manual/position_stepper_manual.ino
+++ test_stepper/position_stepper/position_stepper_manual/position_stepper_manual.ino
@@ -1,3 +1,25 @@
+/* Use this sketch to interactively position a stepper motor 
+ * via a serial port connection with a host PC. Use the host PC 
+ * to issue commands to the Arduino. All commands sent over the 
+ * serial connection must end in '\n' (a newline character). This 
+ * sketch accepts 3 types of commands:
+ * 
+ * 1) An integer: moves the stepper the specified number of steps 
+ * to move the stepper. Positive moves the stepper towards the 
+ * mouse's face, negative moves it in the opposite direction. 
+ * 
+ * 2) An empty string (terminated with '\n'): this will advance
+ * the stepper to the hall effect sensor.
+ * 
+ * 3) q: powers down the stepper motor coils; use to prevent 
+ * overheating and/or when quitting the host PC program. 
+ * 
+ * Note that it is up to the host PC program to do all input
+ * validation.
+ * 
+ * last upadted DDK 2019-08-22
+ */
+ 
 #define STPR_PIN 6
 #define SLP_PIN 9
 #define DIR_PIN 8
@@ -11,7 +33,9 @@
 #define HALL_THRESH 1000
 
 String input;
+String dbg_msg;
 int num_steps;
+bool is_num;
 //int numSteps = floor((REVERSE_ROTATION_DEGREES/360.0) * NUM_STEPS) * MICROSTEP;
 int stpr_powerup_time = 1000;
 int stpr_powerdown_time = 1000;
@@ -41,56 +65,50 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()){
-    input = Serial.readString(); // just to clear the buffer
-    Serial.print("ACK " + input);
-    
+
+    // Get input from host PC:
+    input = Serial.readString(); 
+
     // If user has just pressed 'Enter', rotate to sensor:
-    if(input=="\n"){rotate_to_sensor();}
+    if(input=="\n"){
+       rotate_to_sensor();
+       dbg_msg = "rotating stepper motor to hall effect sensor.\n";
+       }
     
     // If user quits host PC program, power down stepper coils to avoid overheating:
-    else if(input=="q\n"){digitalWrite(SLP_PIN, LOW);}
+    else if(input=="q\n"){
+      digitalWrite(SLP_PIN, LOW);
+      dbg_msg = "powering down coils.\n";
+      }
     
-
+    // Else, asssume the user has entered a number and rotate the requested 
+    // number of steps (will have to validate input on host PC side)
+    else {
       
-    num_steps = input.toInt();
+      // Convert number of steps to int:
+      num_steps = input.toInt();
 
-
-    // Set the direction of the stepper:
-    if(num_steps>0){
-      digitalWrite(DIR_PIN, LOW); // positive -> towards the mouse
+      // Set the direction of the stepper:
+      if(num_steps>0){
+        digitalWrite(DIR_PIN, LOW); // positive -> towards the mouse
+        }
+      else{
+        digitalWrite(DIR_PIN, HIGH); // negative -> away from the mouse      
       }
-    else{
-      digitalWrite(DIR_PIN, HIGH); // negative -> away from the mouse      
-    }
 
-    //Rectify: 
-    num_steps = abs(num_steps);
-
-    // Rotate the requested number of steps: 
-    if(num_steps>0){
-      for (int n = 0; n < num_steps; n++){rotate_one_step();}
-      };
-    }
-
-}
-
-
-char readline(){
-  bool done = false;
-  char msg;
-  char curr;
-  
-  while(~done){
-    if(Serial.available()){curr = Serial.read();}
-
-    // Stop when a newline character has been reached; 
-    if(curr=='\n'){
-      return msg;
+      //Rectify: 
+      num_steps = abs(num_steps);
+      
+      // Rotate the requested number of steps: 
+      if(num_steps>0){
+        for (int n = 0; n < num_steps; n++){rotate_one_step();}
+        };
+        
+      dbg_msg = input;      
       }
-    }
-
-    // Update msg:
-    msg = msg + curr;
+      
+    Serial.print("ACK " + dbg_msg);
+  }
 }
 
 
@@ -103,9 +121,6 @@ void rotate_one_step(){
 
 
 void rotate_to_sensor(){
-  digitalWrite(SLP_PIN, HIGH);
-  delay(stpr_powerup_time);
-    
   digitalWrite(DIR_PIN, LOW);
   //int hall_val = analogRead(HALL_PIN);
   while(analogRead(HALL_PIN)<HALL_THRESH){
@@ -113,11 +128,6 @@ void rotate_to_sensor(){
       //delay(1);
       //hall_val = analogRead(HALL_PIN);
   }
-  Serial.println("stepper extended");
-  stepper_state  = "EXTENDED";
-  
-  delay(stpr_powerdown_time);
-  digitalWrite(SLP_PIN, LOW);
 }
 
 /*
